af47499f0a528c1c1ad04a501cc37bb40bc2ece5
Mickey Gousset <mickeygousset@github.com>
2025-05-22 16:45:09 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DemoCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the GitHub Actions Cache Demo!");
+        }
+    }
+}
